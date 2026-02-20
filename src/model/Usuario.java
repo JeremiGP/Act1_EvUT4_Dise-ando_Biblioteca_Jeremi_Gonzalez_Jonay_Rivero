d77cf4c275cb1b0ccc800c8f8f6b2e4358e33914
JmRivGonza <Jonaymartinrivero@gmail.com>
2026-02-20 14:01:20 +0000

    modificacion minima

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -8,7 +8,7 @@ public class Usuario {
     private String id;
     private String nombre;
     private List<Libro> librosPrestados;
-    private List<Libro> historialPrestamos;
+    private List<Prestamo> historialPrestamos;
     private LocalDate fechaFinSancion;
 
     public Usuario(String id, String nombre) {
@@ -31,7 +31,7 @@ public class Usuario {
         return librosPrestados;
     }
 
-    public List<Libro> getHistorialPrestamos() {
+    public List<Prestamo> getHistorialPrestamos() {
         return historialPrestamos;
     }
 
@@ -39,25 +39,8 @@ public class Usuario {
         return fechaFinSancion;
     }
 
-    public void setId(String id) {
-        if (id == null || id.trim().isEmpty()) {
-            throw new IllegalArgumentException("El ID no puede estar vacío.");
-        }
-        this.id = id;
-    }
-
     public void setFechaFinSancion(LocalDate fecha) {
-        if (fecha == null) {
-            throw new IllegalArgumentException("La fecha de fin de sanción no puede estar vacía.");
-        }
         this.fechaFinSancion = fecha;
-    }
-
-    public void setNombre(String nombre) {
-        if (nombre == null || nombre.trim().isEmpty()) {
-            throw new IllegalArgumentException("El nombre no puede estar vacío.");
-        }
-        this.nombre = nombre;
     }
 
     @Override
